bb2da79b0646e8ce1faca05119a9d8ce70c930aa
Sergey <sergey199433@gmail.com>
2024-08-24 22:32:03 +0400

    fix file json_saver_class.py

--- src/json_saver_class.py
+++ src/json_saver_class.py
@@ -18,7 +18,7 @@ class JSONSaver(Saver):
 
     def insert(self, data):
         """
-        Функция, для добавления информации в файл json.
+        Функция, для добавления информации в файл формата json.
         """
         with open(self.file, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
